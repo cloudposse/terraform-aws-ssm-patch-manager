01f351050169bc052e3924cdb6a5e2cbe1c0ddf3
PePe Amengual <jose.amengual@gmail.com>
2021-06-25 16:13:16 -0700

    Addressing feedback

--- main.tf
+++ main.tf
@@ -53,6 +53,7 @@ resource "aws_ssm_maintenance_window_task" "task_scan_patches" {
       output_s3_key_prefix = "scaning"
 
       service_role_arn = var.role_arn_for_notification
+
       dynamic "notification_config" {
         for_each = var.enable_notification_scan ? [1] : []
         content {
@@ -123,7 +124,6 @@ resource "aws_ssm_maintenance_window_task" "task_install_patches" {
       }
       output_s3_bucket     = local.bucket_id
       output_s3_key_prefix = var.s3_bucket_prefix_install_logs
-
       service_role_arn = var.role_arn_for_notification
 
       dynamic "notification_config" {
