71c406ae29ec693afb74f5aaa8bbd7bf1b7a7477
PePe Amengual <jose.amengual@gmail.com>
2021-07-10 14:45:53 -0700

    Update outputs.tf
    Co-authored-by: nitrocode <nitrocode@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "ssm_patch_log_s3_bucket_id" {
 
 output "ssm_patch_log_s3_bucket_arn" {
   description = "SSM Patch Manager s3 log bucket ARN"
-  value       = local.create_log_bucket ? join("", module.ssm_patch_log_s3_bucket[0].bucket_arn) : ""
+  value       = local.create_log_bucket ? join("", module.ssm_patch_log_s3_bucket.*.bucket_arn) : ""
 }
 
 output "scan_maintenance_window_task_id" {
