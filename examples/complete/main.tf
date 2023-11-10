f5425ac776d2a7e47fc3e43db8fe4c6ae09e9c3f
Max Lobur <max_lobur@outlook.com>
2023-11-10 14:16:01 +0100

    updates

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "cloudposse/vpc/aws"
   version = "2.1.0"
 
-  ipv4_primary_cidr_block = var.vpc_cidr_block
+  ipv4_primary_cidr_block = "172.16.0.0/16"
 
   context = module.this.context
 }
@@ -11,11 +11,11 @@ module "subnets" {
   source  = "cloudposse/dynamic-subnets/aws"
   version = "2.4.1"
 
-  availability_zones   = var.availability_zones
+  availability_zones   = ["us-east-2a", "us-east-2b"]
   vpc_id               = module.vpc.vpc_id
   igw_id               = [module.vpc.igw_id]
   ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
-  nat_gateway_enabled  = false
+  nat_gateway_enabled  = true
   nat_instance_enabled = false
 
   context = module.this.context
