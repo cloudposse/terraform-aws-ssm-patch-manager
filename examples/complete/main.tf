7a55b5266a71ffd0862753232aa2e6cb125eafa6
Matt Gowie <matt@masterpoint.io>
2024-02-23 20:16:35 -0700

    chore: updates example to show patching (closes #40)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -37,9 +37,9 @@ module "ec2_instance" {
   ssm_patch_manager_s3_log_bucket = format("%s-%s-%s-%s", module.this.namespace, module.this.environment, module.this.stage, module.this.name)
 
   tags = {
-    "TOSCAN"  = "true",
-    "TOPATCH" = "true"
+    "Patch Group": "TOPATCH",
   }
+
   context = module.this.context
 
 }
